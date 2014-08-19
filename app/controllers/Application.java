6d4158909d812799d3e930ddbee05b440f5926f4
arpit <arpitmittal108@gmail.com>
2014-08-19 23:17:11 +0530

    add twitter4j dependency in lib

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,15 +1,11 @@
 package controllers;
 
 import models.Task;
-import org.h2.engine.Database;
-import play.*;
 import play.data.Form;
-import play.mvc.*;
-import play.api.db.*;
+import play.mvc.Controller;
+import play.mvc.Result;
+import play.mvc.Results;
 import twitter4j.*;
-
-
-import views.html.*;
 
 public class Application extends Controller {
     static Form<Task> taskForm = Form.form(Task.class);
@@ -43,40 +39,6 @@ public class Application extends Controller {
     }
 
     public static Result getTweets(String searchString) throws TwitterException{
-        TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
-        StatusListener listener = new StatusListener() {
-            @Override
-            public void onStatus(Status status) {
-                System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText());
-            }
-
-            @Override
-            public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
-                System.out.println("Got a status deletion notice id:" + statusDeletionNotice.getStatusId());
-            }
-
-            @Override
-            public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
-                System.out.println("Got track limitation notice:" + numberOfLimitedStatuses);
-            }
-
-            @Override
-            public void onScrubGeo(long userId, long upToStatusId) {
-                System.out.println("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
-            }
-
-            @Override
-            public void onStallWarning(StallWarning warning) {
-                System.out.println("Got stall warning:" + warning);
-            }
-
-            @Override
-            public void onException(Exception ex) {
-                ex.printStackTrace();
-            }
-        };
-        twitterStream.addListener(listener);
-        twitterStream.sample();
         return Results.TODO;
     }
 
